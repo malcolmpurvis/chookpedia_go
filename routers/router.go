6c6f219ab4ee0045df48bde3a0a461643b526d32
Malcolm Purvis <malcolm@purvis.id.au>
2024-08-30 18:47:08 +1000

    Add the beego example template.

---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"chookpedia_go/controllers"
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
